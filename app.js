dc06781917f44e6af60877f20e695d055a0a8ee2
minhtu-3011 <tudoi2002@gmail.com>
2025-02-26 15:33:35 +0700

    cai dat cac handle truy xuat CSDL

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const contactsRouter = require("./app/routes/contact.route");
 const ApiError = require("./app/api-error");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
-
+app.get("/", (req, res) => {
+    res.json({ message: "welcome to contact book application."});
+});
 //handle 404 response
 app.use((req, res, next) => {
     return next(new ApiError(404, "Resource not found"));
@@ -19,11 +20,5 @@ app.use((err, req, res, next) => {
         message: err.message || "Internal Server Error",
     });
 });
-
-app.get("/", (req, res) => {
-    res.json({ message: "welcome to contact book application." });
-});
-
-
 
 module.exports = app;
